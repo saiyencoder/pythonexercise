e19eb8444728c7bcc7df80192a65fc115da1408c
Jesus Santizo <jsantizo@selectrehab.com>
2018-11-20 20:40:51 -0600

    Added validation for user inputs and refactored code

--- python_calculator/calculator.py
+++ python_calculator/calculator.py
@@ -1,17 +1,58 @@
 # Python Calculator
 
-def add(num_1, num_2):
-  return num_1 + num_2
+def valid_float_input():
+  while True:
+    try:
+      num_1 = float(input("First number: "))
+      num_2 = float(input("Second number: "))
+      return num_1, num_2
+    except:
+      print("Sorry but that is an invalid input.")
+      print("Please enter a whole or decimal number to run the operation. Try again.\n")
+      continue
 
-def sub(num_1, num_2):
-  return num_1 - num_2
+def add():
+  num_1,num_2 = valid_float_input() 
+  sum = num_1 + num_2
+  print("%d + %d = %d" % (num_1, num_2, sum))
+  
+def sub():
+  num_1,num_2 = valid_float_input() 
+  difference = num_1 - num_2
+  print("%d - %d = %d" % (num_1, num_2, difference))
 
-def mul(num_1, num_2):
-  return num_1 * num_2
+def mul():
+  num_1,num_2 = valid_float_input() 
+  product = num_1 * num_2
+  print("%d x %d = %d" % (num_1, num_2, product))
 
-def div(num_1, num_2):
-  return num_1 / num_2
+def div():
+  num_1,num_2 = valid_float_input() 
+  quotient = num_1 / num_2
+  print("%d / %d = %d" % (num_1, num_2, quotient))
 
+def valid_operation_input():
+  while True:
+    try:
+      option = int(input("Operation (Choose #): "))
+      if option > 0 and option <= 4:
+        return option
+      else:
+        print("Sorry but that is not a choice. Try again.\n")
+        continue
+    except ValueError:
+      print("Sorry but that was not a number!\n")
+      continue
+
+def calculator(choice):
+  if choice == 1:
+    add()
+  elif choice == 2:
+    sub()
+  elif choice == 3:
+    mul()
+  elif choice == 4:
+    div()
 
 print("Welcome to the Python Calculator!")
 print()
@@ -20,21 +61,4 @@ print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-operation = int(input("Operation (Choose #): "))
-
-if operation == 1:
-  num_1 = int(input("First number: "))
-  num_2 = int(input("Second number: "))
-  print("%d + %d = %d" % (num_1, num_2, add(num_1,num_2)))
-elif operation == 2:
-  num_1 = int(input("First number: "))
-  num_2 = int(input("Second number: "))
-  print("%d - %d = %d" % (num_1, num_2, sub(num_1,num_2)))
-elif operation == 3:
-  num_1 = int(input("First number: "))
-  num_2 = int(input("Second number: "))
-  print("%d x %d = %d" % (num_1, num_2, mul(num_1,num_2)))
-elif operation == 4:
-  num_1 = int(input("First number: "))
-  num_2 = int(input("Second number: "))
-  print("%d / %d = %d" % (num_1, num_2, div(num_1,num_2)))
+calculator(valid_operation_input())
